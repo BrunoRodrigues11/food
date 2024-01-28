a0afb5955308d7a57efeea62b29acfdf6fa5c840
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-01-28 01:28:13 -0300

    feat: History Page

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import "./navbar.css";
 import logo from "../../assets/logo.png";
 import bag from "../../assets/bag.png";
 import Cart from "../cart/cart";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
   // Dispara um evento para abrir a sidebar
@@ -18,7 +19,7 @@ function Navbar(props) {
         // Se a propriedade showMenu for verdadeira, então renderiza o menu
         props.showMenu ? (
           <div className="menu">
-            <a href="#">Histórico</a>
+            <Link to="/historico">Histórico</Link>
             <button className="btn btn-red" type="button" onClick={openSidebar}>
               <img src={bag} className="icon" />
               Sacola
--- src/pages/historico/historico.jsx
+++ src/pages/historico/historico.jsx
@@ -1,5 +1,58 @@
+import Navbar from "../../components/navbar/navbar.jsx";
+import "./historico.css";
+
 function Historico() {
-  return <h1>Histórico...</h1>;
+  const pedidos = [
+    { id_pedido: 541561, dt: "14/01/2024", total: 150.3 },
+    { id_pedido: 841562, dt: "15/01/2024", total: 99 },
+    { id_pedido: 887453, dt: "18/01/2024", total: 119.9 },
+    { id_pedido: 985417, dt: "19/01/2024", total: 37.2 },
+    { id_pedido: 995411, dt: "20/01/2024", total: 49.9 },
+    { id_pedido: 985852, dt: "21/01/2024", total: 70.0 },
+    { id_pedido: 998524, dt: "22/01/2024", total: 50.0 },
+  ];
+  return (
+    <>
+      <Navbar showMenu={true} />
+      <div className="container">
+        <div className="titulo text-center">
+          <h1>Histórico de Pedidos</h1>
+        </div>
+        <div className="box-pedido">
+          <table className="table">
+            <tr>
+              <td>
+                <strong>Pedido 02215</strong>
+              </td>
+              <td className="text-light">28/01/2024</td>
+              <td className="text-red">R$ 500,00</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Pedido 02215</strong>
+              </td>
+              <td className="text-light">28/01/2024</td>
+              <td className="text-red">R$ 500,00</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Pedido 02215</strong>
+              </td>
+              <td className="text-light">28/01/2024</td>
+              <td className="text-red">R$ 500,00</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Pedido 02215</strong>
+              </td>
+              <td className="text-light">28/01/2024</td>
+              <td className="text-red">R$ 500,00</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Historico;
