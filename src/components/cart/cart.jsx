39ba867aab617d251dd2f2d63f18eba52c07f8ea
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-01-28 01:06:41 -0300

    feat: Checkout Page

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -2,17 +2,27 @@ import { useEffect, useState } from "react";
 import { Dock } from "react-dock";
 import ProcutoCart from "../produto-cart/produto-cart.jsx";
 import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
   // O estado "show" controla se a sidebar está aberta ou fechada
   const [show, setShow] = useState(false);
 
+  // O hook "useNavigate" é usado para navegar entre as páginas
+  const navigate = useNavigate();
+
+  // O hook "useEffect" é usado para executar um código quando o componente é montado
   useEffect(() => {
     // Fica ouvindo o evento "openSidebar" e abre a sidebar quando ele é disparado
     window.addEventListener("openSidebar", () => {
       setShow(true);
     });
   }, []);
+
+  // Função para navegar para a página de checkout
+  function checkout() {
+    navigate("/checkout");
+  }
 
   return (
     <Dock
@@ -41,7 +51,7 @@ function Cart() {
           </span>
         </div>
         <div>
-          <button type="button" className="btn btn-checkout">
+          <button type="button" className="btn btn-checkout" onClick={checkout}>
             Finalizar Pedido
           </button>
         </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo.png";
 import bag from "../../assets/bag.png";
 import Cart from "../cart/cart";
 
-function Navbar() {
+function Navbar(props) {
   // Dispara um evento para abrir a sidebar
   function openSidebar() {
     const event = new CustomEvent("openSidebar");
@@ -13,13 +13,20 @@ function Navbar() {
   return (
     <div className="navbar">
       <img src={logo} alt="Logotipo" className="logotipo" />
-      <div className="menu">
-        <a href="#">Histórico</a>
-        <button className="btn btn-red" type="button" onClick={openSidebar}>
-          <img src={bag} className="icon" />
-          Sacola
-        </button>
-      </div>
+
+      {
+        // Se a propriedade showMenu for verdadeira, então renderiza o menu
+        props.showMenu ? (
+          <div className="menu">
+            <a href="#">Histórico</a>
+            <button className="btn btn-red" type="button" onClick={openSidebar}>
+              <img src={bag} className="icon" />
+              Sacola
+            </button>
+          </div>
+        ) : null
+      }
+
       <Cart />
     </div>
   );
